de6a421812f0d95d9fcce483d2e3321a571f51e3
CrystalFaith <crystallambert@crystals-mbp.compumentor.org>
2017-07-06 12:36:09 -0700

    launch

---
+++ build/firebase-messaging-sw.js
@@ -0,0 +1,32 @@
+// Give the service worker access to Firebase Messaging.
+// Note that you can only use Firebase Messaging here, other Firebase libraries
+// are not available in the service worker.
+importScripts('https://www.gstatic.com/firebasejs/3.9.0/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/3.9.0/firebase-messaging.js');
+
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyD-np5USZAOXmA51TB8EmNcPPYCnffOmjI",
+  authDomain: "villageapp-6bbe4.firebaseapp.com",
+  databaseURL: "https://villageapp-6bbe4.firebaseio.com",
+  projectId: "villageapp-6bbe4",
+  storageBucket: "villageapp-6bbe4.appspot.com",
+  messagingSenderId: "955973472886"
+};
+firebase.initializeApp(config);
+
+// Retrieve an instance of Firebase Messaging so that it can handle background
+// messages.
+const messaging = firebase.messaging();
+
+messaging.setBackgroundMessageHandler(function(payload) {
+  console.log('[firebase-messaging-sw.js] Received background message ', payload);
+  // Customize notification here
+  const notificationTitle = 'Background Message Title';
+  const notificationOptions = {
+    body: 'Background Message body.',
+  };
+
+  return self.registration.showNotification(notificationTitle,
+      notificationOptions);
+});
